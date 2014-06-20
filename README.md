70b14b6063a39a7ea7d77218aed06b5278532ca3
mistic100 <mistic@strangeplanet.fr>
2014-06-20 20:17:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# AngularJS jQCloud
+
+Simple AngularJS directive for [jQCloud](https://github.com/mistic100/jQCloud), a beautiful words cloud generator.
+
+## Usage
+
+```html
+<jqcloud words="words" width="500" height="350" steps="7"></jqcloud>
+
+<script>
+  var app = angular.module('app', [
+    'angular-jqcloud'
+  ]);
+
+  app.controller('controller', function($scope) {
+    $scope.words = [/* ... */];
+  });
+</script>
+```
+
+Consult [jQCloud documentation](http://mistic100.github.io/jQCloud) for full options.
---
+++ angular-jqcloud.js
@@ -0,0 +1,49 @@
+/*!
+ * Angular jQCloud
+ * For jQCloud 2 (https://github.com/mistic100/jQCloud) 
+ * Copyright 2014 Damien "Mistic" Sorel (http://www.strangeplanet.fr)
+ * Licensed under MIT (http://opensource.org/licenses/MIT)
+ */
+
+angular.module('angular-jqcloud', []).directive('jqcloud', ['$parse', function($parse) {
+  // get existing options
+  var defaults = jQuery.fn.jQCloud.defaults.get(),
+      jqcOptions = [];
+  
+  for (var opt in defaults) {
+    if (defaults.hasOwnProperty(opt)) {
+      jqcOptions.push(opt);
+    }
+  }
+  
+  return {
+    restrict: 'E',
+    template: '<div></div>',
+    replace: true,
+    scope: {
+      words: '=words'
+    },
+    link: function($scope, $elem, $attr) {
+      var options = {};
+      
+      for (var i=0, l=jqcOptions.length; i<l; i++) {
+        var opt = jqcOptions[i];
+        if ($attr[opt]!=undefined) {
+          options[opt] = $parse($attr[opt])();
+        }
+      }
+      
+      $elem.jQCloud($scope.words, options);
+      
+      $scope.$watchCollection('words', function() {
+        $scope.$evalAsync(function() {
+          $elem.jQCloud('update', $scope.words);
+        });
+      });
+    
+      $elem.bind('$destroy', function() {
+        $elem.jQCloud('destroy');
+      });
+    }
+  };
+}]);
